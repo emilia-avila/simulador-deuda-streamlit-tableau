0b7d7e21c1260254ad34722fb2677990d30f4d37
Emilia Avila <emiavila97@gmail.com>
2026-03-18 15:22:05 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,21 +1,23 @@
 import streamlit as st
 from utils.layout import firma_sidebar
 
-# Firma global en sidebar
+# Firma en la barra lateral
 firma_sidebar()
 
 st.title("Simulador de Deuda")
 
 st.write("Bienvenido/a 👋")
 
-st.write("""
-Esta aplicación te permitirá:
+st.write(
+    """
+    Esta aplicación te permitirá:
 
-- Entender los conceptos clave de una deuda
-- Generar tablas de amortización (Francesa y Alemana)
-- Comparar escenarios
-- Calcular la tasa de interés a partir de la cuota
-- Conectar tus resultados a un dashboard en Tableau
-""")
+    - Entender los conceptos clave de una deuda
+    - Generar tablas de amortización (Francesa y Alemana)
+    - Comparar escenarios
+    - Calcular la tasa de interés a partir de la cuota
+    - Conectar tus resultados a un dashboard en Tableau
+    """
+)
 
 st.info("Usa el menú lateral para navegar por las secciones")
