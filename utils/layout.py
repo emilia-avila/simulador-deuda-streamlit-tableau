646e57927132f4bfed3e67bd838a7afb3bdb89c7
Emilia Avila <emiavila97@gmail.com>
2026-03-18 15:23:36 -0500

    Update layout.py

--- utils/layout.py
+++ utils/layout.py
@@ -4,8 +4,8 @@ def firma_sidebar():
 
     st.sidebar.markdown(
         """
-        <div style="font-size: 12px; color: #9CA3AF;">
-            Elaborado por: Emilia Ávila | Data Analytics
+        <div style="font-size: 12px; color: #8A8A8A;">
+            <b>Elaborado por:</b> Emilia Ávila | Data Analytics
         </div>
         <a href="https://www.linkedin.com/in/emilia-avila-vasconez" target="_blank">
             <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" 
